10b14ab240a30f66802f0dc78c8661805f5b389a
Quyln <kayq9999@gmail.com>
2023-08-13 19:17:44 +0700

    display data firestore
    show listview done

--- lib/Auth/home_page.dart
+++ lib/Auth/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaselearn/get_user_name.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,22 +13,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  List<String> docsID = [];
+
+  Future getDocsID() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((element) {
+              print(element.reference);
+              docsID.add(element.reference.id);
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(user.email!),
+        actions: [
+          GestureDetector(
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Icon(Icons.logout))
+        ],
+      ),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Login Successful ' + user.email!),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: Text('Sign Out'),
-            color: Colors.blue,
-          ),
-          // ElevatedButton(onPressed: () {}, child: Text('go to detail'))
+          Expanded(
+            child: FutureBuilder(
+              future: getDocsID(),
+              builder: (context, snapshot) => ListView.builder(
+                  itemCount: docsID.length,
+                  itemBuilder: (context, index) => ListTile(
+                        title: GetUserName(documentID: docsID[index]),
+                      )),
+            ),
+          )
         ],
       )),
     );
